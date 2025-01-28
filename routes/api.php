962f2f5cb05c7b2d75650add25b5b5a1d3db43fc
Blake Neese <blake@gearboxgo.com>
2025-01-27 21:21:22 -0500

    Prefix route group

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\NYTBestSellersController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->name('api.v1.')->group(function () {
     Route::get('/nyt/best-sellers', NYTBestSellersController::class)->name('nyt.best-sellers');
 });
